142c16f854d20dd5143fa21bcf7e3e233eada43b
kassraniroumand <kassra.niroumand@gmail.com>
2025-06-06 09:36:11 +0100

    update main 5

--- src/api/endpoint.py
+++ src/api/endpoint.py
@@ -28,7 +28,7 @@ router = APIRouter()
 @router.get("/")
 async def ping():
     """simple health check used by ecs"""
-    return {"message": "pong new 4"}
+    return {"message": "pong new 5"}
 
 
 @router.post("/upload-pdf")
